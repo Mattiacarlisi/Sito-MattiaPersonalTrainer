7a7f3bfd03afbe3a79dfa449a0223eecfc8e7e84
mattia <carlisimattia@gmail.com>
2024-08-14 15:26:06 +0200

    rrr

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.toggle_btn');
+    const dropdownMenu = document.querySelector('.dropdown_menu');
+  
+    // Aggiunge l'evento di click al pulsante di toggle
+    toggleButton.addEventListener('click', () => {
+      // Toglie o aggiunge la classe 'active' al menu a discesa
+      dropdownMenu.classList.toggle('active');
+    });
+  });
+  
